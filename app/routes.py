a081594aab4a92c7723a5bfb5df48c5593730b1c
colinsford <cford22@sycamores.indstate.edu>
2023-04-13 23:15:22 -0400

    commit

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Colin'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - TFord Photo</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
+    return render_template('index.html', title='Home', user=user)
