6784f86c0552765a79f6b86ec4030b90793b332b
colinsford <cford22@sycamores.indstate.edu>
2023-04-18 23:06:30 -0400

    got more things working

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,57 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, IntegerField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app.models import User, Client, MailingAddress, Shoot
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+        
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+        
+class ClientForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    phone = StringField('Phone')
+    payment_information = StringField('Payment Information')
+    street = StringField('Street')
+    city = StringField('City')
+    state = StringField('State')
+    postal_code = StringField('Postal Code')
+    country = StringField('Country')
+    submit = SubmitField('Submit')
+
+class ShootInfo(FlaskForm):
+    locations = StringField('Location')
+    client_id = SelectField(u'Client', coerce=int)
+    time = StringField('Time/Date')
+    prompt = StringField('Shoot Prompt')
+    equipment = StringField('Equipment')
+    num_photos_requested = IntegerField('Number of Photos Requested')
+    model_release = StringField('Model Release?')
+    branding = StringField('Link to Branding / Graphics')
+    
+    def edit_client(request, id):
+        client = Client.query.get(id)
+        form = ShootInfo(request.POST, obj=client)
+        form.client_id.choices = [(c.id, c.first_name, c.last_name) for c in Client.query.order_by('name')]
+
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,16 @@
-from flask import render_template, flash, redirect, url_for
-from app import app
-from app.forms import LoginForm
-from flask_login import current_user, login_user
-from app.models import User
+from flask import render_template, flash, redirect, url_for, request
+from app import app, db
+from app.forms import LoginForm, RegistrationForm, ClientForm, ShootInfo
+from flask_login import current_user, login_user, login_required
+from app.models import User, Client, MailingAddress, Shoot
+from flask_login import logout_user
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'Colin'}
-    return render_template('index.html', title='Home', user=user)
+    return render_template('index.html', title='Home')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -21,5 +23,43 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/clientform', methods=['GET','POST'])
+def clientform():
+    form = ClientForm()
+    if form.validate_on_submit():
+        client = Client(first_name=form.first_name.data, last_name=form.last_name.data, email=form.email.data, phone=form.phone.data, payment_information=form.payment_information.data)
+        mailing_address = MailingAddress(street=form.street.data, city=form.city.data, state=form.state.data, postal_code=form.postal_code.data, country=form.country.data, client_id=client.id)
+        db.session.add(client)
+        db.session.add(mailing_address)
+        db.session.commit()
+        flash('Client Information Entered Successfully')
+        return redirect(url_for('index'))
+    return render_template('clientform.html', title='Client Information', form=form)
+
+@app.route('/shootform', methods=['GET', 'POST'])
+def shoot
